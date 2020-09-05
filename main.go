72b12993dffe2d879325c5410bd626d7d8a6bcc2
Nopekick <tgkamm@yahoo.com>
2020-09-04 21:28:36 -0700

    better quit

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"os/exec"
+	"os/signal"
 	"regexp"
 	"strings"
 	"syscall"
@@ -13,6 +15,7 @@ import (
 
 func main() {
 	cmd := "ifconfig | grep netmask"
+
 	out, err := exec.Command("bash", "-c", cmd).Output()
 
 	if err != nil {
@@ -34,16 +37,34 @@ func main() {
 		panic(err)
 	}
 
+	endChan := make(chan os.Signal)
+	signal.Notify(endChan, os.Interrupt)
+
+	go endCheck(endChan, pc)
 	go Listen(pc)
 
 	for {
 		bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
+		if string(bytePassword) == "exit" {
+			exec.Command("stty echo").Run()
+			os.Exit(0)
+		}
 		if string(bytePassword) != "" {
 			_, err2 := pc.WriteTo([]byte(string(bytePassword)), addr)
 			if err2 != nil {
 				panic(err2)
 			}
 		}
+	}
+}
+
+func endCheck(endChan chan os.Signal, pc net.PacketConn) {
+	select {
+	case sig := <-endChan:
+		exec.Command("stty echo").Run()
+		pc.Close()
+		fmt.Printf("<<%s>> Leaving chat...\n", sig)
+		os.Exit(0)
 	}
 }
 
